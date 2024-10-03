2d7ca06eff3029e7b85fe4b16774be5218ca7abc
honorine22 <higiranez@gmail.com>
2024-10-03 14:32:19 +0200

    servers addresses changed

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,7 +4,7 @@
 
 from fabric.api import put, run, env
 from os.path import exists
-env.hosts = ['54.227.179.101', '3.90.204.71']
+env.hosts = ['54.226.153.95', '	54.226.0.24']
 
 
 def do_deploy(archive_path):
