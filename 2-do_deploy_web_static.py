167645c4026722fa7ab9a5fc23a4ed08e8dc31aa
honorine22 <higiranez@gmail.com>
2024-10-04 09:56:58 +0200

    permission for setup web static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,50 +1,54 @@
 #!/usr/bin/python3
-"""Fabric Script that distributes an archive."""
+"""Fabric script to deploy an archive to web servers."""
 
 from fabric import Connection, task
 from os.path import exists
+import os
 
 # Define the hosts
-env_hosts = ['100.27.187.5', '54.167.59.7']
+env.hosts = ['<IP web-01>', '<IP web-02>']  # Replace with your server IPs
+env.user = 'your_username'  # Replace with your SSH username
+env.key_filename = '~/.ssh/id_rsa'  # Default path to your SSH private key
 
 @task
-def do_deploy(c, archive_path):
+def do_deploy(archive_path):
     """Distributes an archive to web servers."""
     if not exists(archive_path):
         print(f"Archive path {archive_path} does not exist.")
         return False
 
     try:
-        file_n = archive_path.split("/")[-1]
+        file_n = os.path.basename(archive_path)
         no_ext = file_n.split(".")[0]
         path = "/data/web_static/releases/"
         
         # Upload the archive to the server
-        c.put(archive_path, '/tmp/')
-        
-        # Create the target directory
-        c.run('mkdir -p {}{}/'.format(path, no_ext))
-        
-        # Extract the archive
-        c.run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        
-        # Clean up the temporary file
-        c.run('rm /tmp/{}'.format(file_n))
-        
-        # Move the contents of web_static to the new release folder
-        c.run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        
-        # Remove the now-empty web_static directory
-        c.run('rm -rf {}{}/web_static'.format(path, no_ext))
-        
-        # Remove the current symlink
-        c.run('rm -rf /data/web_static/current')
-        
-        # Create a new symlink to the new version
-        c.run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        for host in env.hosts:
+            conn = Connection(host=host, user=env.user, connect_kwargs={"key_filename": env.key_filename})
+            
+            # Upload the archive to /tmp/
+            conn.put(archive_path, '/tmp/')
+            
+            # Create the directory to extract the archive
+            conn.run('mkdir -p {}{}/'.format(path, no_ext))
+            
+            # Uncompress the archive to the specified directory
+            conn.run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+            
+            # Delete the archive from the server
+            conn.run('rm /tmp/{}'.format(file_n))
+            
+            # Remove the current symlink
+            conn.run('rm -rf /data/web_static/current')
+            
+            # Create a new symlink to the new version
+            conn.run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         
         print("Deployment successful!")
         return True
     except Exception as e:
         print(f"An error occurred during deployment: {e}")
         return False
+
+# To execute the task, use:
+# fab -H <IP web-01>,<IP web-02> do_deploy:/path/to/archive
